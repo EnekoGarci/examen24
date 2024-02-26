479fa6f444f41d947454f4016318d68626207829
Eneko <damj04.2024@gmail.com>
2024-02-26 13:19:30 +0100

    Proyecto documentado

--- src/examen/RockPaperScissors.java
+++ src/examen/RockPaperScissors.java
@@ -1,15 +1,22 @@
 package examen;
 
 import java.util.*;
-
+/**Clase que simula el juego de piedra papel y tijera.
+ * @param Keyboard Un entero inicializado a 0
+ * @param scanner Un scanner
+ * @param optionList Un arraylist inicializado con tres valores ("Rock", "Paper" y "Scissors")*/
 public class RockPaperScissors {
 	private static int keyboard = 0;
 	private static Scanner scanner = new Scanner(System.in);
 	public static List<String> optionList = Arrays.asList("Rock", "Paper", "Scissors");
 
+	/**@constructor constructor por defecto */
 	public RockPaperScissors() {
 	}
 
+	/**Método main que ejecuta un bucle que se ejecuta hasta que pongamos una opción correcta (entre 1 y 3 incluidos)
+	 * almacena nuestra decision en la variable userChoice y nos muestra nuestra decision y la de computer
+	 * para posteriormente llamar al metodo checkGame*/
 	public static void main(String[] args) {
 		do {
 			System.out.println("Welcome to Rock, Paper, Scissors!");
@@ -24,6 +31,11 @@ public class RockPaperScissors {
 		scanner.close();
 	}
 
+	/**Clase que según los resultados del usuario y el computer, escribe el resultado obtenido
+	 * @param userChoice Es la opción que elige el usuario
+	 * @param computerChoice Es la opción generada aleatoriamente por el computer
+	 * @param gameResult Es una variable que contiene el resultado
+	 * @return Devuelve la variable que contiene el resultado*/
 	public static String checkGame(String userChoice, String computerChoice) {
 		String gameResult = "";
 		if (userChoice.equals(computerChoice)) {
